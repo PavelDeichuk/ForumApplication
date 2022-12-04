58b101ced9fe3c557c6b62a07387ad91df08a97b
pavel <deychuk@inbox.ru>
2022-12-04 23:33:38 +0300

    Add annotation service

--- src/main/java/com/pavel/forumapplication/Service/impl/UsersDetailServiceImpl.java
+++ src/main/java/com/pavel/forumapplication/Service/impl/UsersDetailServiceImpl.java
@@ -10,11 +10,12 @@ import com.pavel.forumapplication.Service.UsersDetailService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class UsersDetailServiceImpl implements UsersDetailService {
 
     private final UsersRepository usersRepository;
