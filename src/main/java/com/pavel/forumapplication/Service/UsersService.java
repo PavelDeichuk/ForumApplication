98cf6555a92c8f3ee56fb95f9afcde21f5457f1e
pavel <deychuk@inbox.ru>
2022-12-04 23:07:05 +0300

    Change UsersService

--- src/main/java/com/pavel/forumapplication/Service/UsersService.java
+++ src/main/java/com/pavel/forumapplication/Service/UsersService.java
@@ -2,17 +2,26 @@ package com.pavel.forumapplication.Service;
 
 import com.pavel.forumapplication.Dto.UsersDto;
 import com.pavel.forumapplication.Entity.UsersEntity;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface UsersService {
-    List<UsersDto> GetAllUsers();
+    List<UsersDto> GetAllUsers(int page, int size);
 
     UsersDto GetUserById(Long user_id);
 
     UsersDto ActivateAccount(String activation);
 
     UsersDto CreateUser(UsersEntity usersEntity);
+
+    UsersDto ChangePassword(String email);
+
+    UsersDto ChangeEmail(String email);
+
+    UsersDto ResetEmail(UsersEntity usersEntity, String email_token);
+
+    UsersDto ResetPassword(UsersEntity usersEntity, String pass_token);
 
     UsersDto EditUser(Long user_id, UsersEntity usersEntity);
 
