bc3e6bab46d32f9cc1f88970b6a6298d8ad75237
pavel <deychuk@inbox.ru>
2022-12-04 23:55:34 +0300

    Add UserDetailService in SecurityFilterChain

--- src/main/java/com/pavel/forumapplication/Configuration/SecurityConfiguration.java
+++ src/main/java/com/pavel/forumapplication/Configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.pavel.forumapplication.Configuration;
 
+import com.pavel.forumapplication.Security.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,15 +10,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final UserDetailService userDetailService;
+
+    public SecurityConfiguration(UserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf()
                 .disable()
+                .userDetailsService(userDetailService)
                 .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .build();
     }
+
 }
