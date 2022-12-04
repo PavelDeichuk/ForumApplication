8677730b264b9a3adc3fb492fca2b3dec1ff2c5b
pavel <deychuk@inbox.ru>
2022-12-04 23:07:51 +0300

    Add UsersDetailEntity

---
+++ src/main/java/com/pavel/forumapplication/Entity/UsersDetailEntity.java
@@ -0,0 +1,37 @@
+package com.pavel.forumapplication.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users-detail")
+@DynamicUpdate
+@DynamicInsert
+public class UsersDetailEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private int age;
+
+    private String description;
+
+    private LocalDateTime last_online;
+
+    @Transient
+    private boolean status;
+
+}
