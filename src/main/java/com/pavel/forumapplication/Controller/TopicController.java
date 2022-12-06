4213687794f41b004ebce00f193d70bd31c35b4f
pavel <deychuk@inbox.ru>
2022-12-06 12:54:59 +0300

    Add TopicController

---
+++ src/main/java/com/pavel/forumapplication/Controller/TopicController.java
@@ -0,0 +1,48 @@
+package com.pavel.forumapplication.Controller;
+
+import com.pavel.forumapplication.Dto.TopicDto;
+import com.pavel.forumapplication.Entity.TopicEntity;
+import com.pavel.forumapplication.Service.TopicService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/topic")
+public class TopicController {
+    private static final String TOPIC_ID = "/{topic_id}";
+
+    private final TopicService topicService;
+
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<TopicDto> GetAllTopic(@RequestParam(value = "page", defaultValue = "page") int page, @RequestParam(value = "size", defaultValue = "10") int size){
+        return topicService.GetAllTopic(page,size);
+    }
+
+    @RequestMapping(value = TOPIC_ID, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public TopicDto GetTopicById(@PathVariable Long topic_id){
+        return topicService.GetTopicById(topic_id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public TopicDto CreateTopic(@RequestBody TopicEntity topicEntity){
+        return topicService.CreateTopic(topicEntity);
+    }
+
+    @RequestMapping(value = TOPIC_ID, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public TopicDto EditTopic(@PathVariable Long topic_id,
+                              @RequestBody TopicEntity topicEntity){
+        return topicService.EditTopic(topic_id,topicEntity);
+    }
+
+    @RequestMapping(value = TOPIC_ID, method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public TopicDto DeleteTopic(@PathVariable Long topic_id){
+        return topicService.DeleteTopic(topic_id);
+    }
+}
