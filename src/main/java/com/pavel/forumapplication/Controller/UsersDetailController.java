6a565205bae97b0c533325dca53ff210077d4df2
pavel <deychuk@inbox.ru>
2022-12-07 21:59:46 +0300

    Change UsersDetailController

--- src/main/java/com/pavel/forumapplication/Controller/UsersDetailController.java
+++ src/main/java/com/pavel/forumapplication/Controller/UsersDetailController.java
@@ -3,17 +3,16 @@ package com.pavel.forumapplication.Controller;
 import com.pavel.forumapplication.Dto.UsersDetailDto;
 import com.pavel.forumapplication.Entity.UsersDetailEntity;
 import com.pavel.forumapplication.Service.UsersDetailService;
-import org.mapstruct.control.MappingControl;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/users-detail")
+@RequestMapping("/api/v1/user-detail")
 public class UsersDetailController {
 
-    private static final String USER_ID = "/{user_id}";
+    private static final String USER_DETAIL_ID = "/{user_id}";
     private final UsersDetailService usersDetailService;
 
     public UsersDetailController(UsersDetailService usersDetailService) {
@@ -21,25 +20,28 @@ public class UsersDetailController {
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<UsersDetailDto> GetAllUsersDetail(@RequestParam(value = "number", defaultValue = "1") int number,
-                                                  @RequestParam(value = "size", defaultValue = "10") int size) {
-        return usersDetailService.GetAllUsersDetail(number,size);
+    public List<UsersDetailDto> GetAllUsersDetail(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                  @RequestParam(value = "size", defaultValue = "10") int size){
+        return usersDetailService.GetAllUsersDetail(page,size);
     }
 
-    @RequestMapping(value = USER_ID, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = USER_DETAIL_ID, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public UsersDetailDto GetUserDetailById(@PathVariable Long user_id){
-        return usersDetailService.GetUserDetailById(user_id);
+        return usersDetailService.GetUsersDetailById(user_id);
     }
 
-    @RequestMapping(value = USER_ID, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public UsersDetailDto CreateUserDetail(@PathVariable Long user_id,
-                                           @RequestBody UsersDetailEntity usersDetailEntity){
-        return usersDetailService.CreateUserDetail(user_id, usersDetailEntity);
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UsersDetailDto CreateUserDetail(@RequestBody UsersDetailEntity usersDetailEntity){
+        return usersDetailService.CreateUserDetail(usersDetailEntity);
     }
 
-    @RequestMapping(value = USER_ID, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public UsersDetailDto EditUserDetail(@PathVariable Long user_id,
-                                         @RequestBody UsersDetailEntity usersDetailEntity){
-        return usersDetailService.EditUserDetail(user_id,usersDetailEntity);
+    @RequestMapping(value = USER_DETAIL_ID, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UsersDetailDto EditUser(@PathVariable Long user_id, @RequestBody UsersDetailEntity usersDetailEntity){
+        return usersDetailService.EditUserDetail(user_id, usersDetailEntity);
+    }
+
+    @RequestMapping(value = USER_DETAIL_ID, method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UsersDetailDto DeleteUser(@PathVariable Long user_id){
+        return usersDetailService.DeleteUserDetail(user_id);
     }
 }
