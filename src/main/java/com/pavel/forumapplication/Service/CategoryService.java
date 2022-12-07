c8230ab4891e5c3c5ce22628672dccb31479a993
pavel <deychuk@inbox.ru>
2022-12-07 21:24:16 +0300

    Update CategoryService

--- src/main/java/com/pavel/forumapplication/Service/CategoryService.java
+++ src/main/java/com/pavel/forumapplication/Service/CategoryService.java
@@ -1,5 +1,18 @@
 package com.pavel.forumapplication.Service;
 
+import com.pavel.forumapplication.Dto.CategoryDto;
+import com.pavel.forumapplication.Entity.CategoryEntity;
+
+import java.util.List;
+
 public interface CategoryService {
 
+    List<CategoryDto> GetAllCategories(int page, int size);
+    CategoryDto GetCategoryByName(String name);
+
+    CategoryDto CreateCategory(CategoryEntity categoryEntity);
+
+    CategoryDto EditCategory(Long category_id, CategoryEntity categoryEntity);
+
+    CategoryDto DeleteCategory(Long category_id);
 }
