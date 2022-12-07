3f51489ce3be06cfe800db271e36994631a00833
pavel <deychuk@inbox.ru>
2022-12-07 22:01:41 +0300

    Delete query findbyusersentity

--- src/main/java/com/pavel/forumapplication/Repository/UsersDetailRepository.java
+++ src/main/java/com/pavel/forumapplication/Repository/UsersDetailRepository.java
@@ -1,10 +1,7 @@
 package com.pavel.forumapplication.Repository;
 
 import com.pavel.forumapplication.Entity.UsersDetailEntity;
-import com.pavel.forumapplication.Entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersDetailRepository extends JpaRepository<UsersDetailEntity,Long> {
-    UsersDetailEntity findByUsersEntity(UsersEntity usersEntity);
-
 }
