3d5ae3ca854980d3a3752f832732b3399224219d
pavel <deychuk@inbox.ru>
2022-12-13 19:17:51 +0300

    Change UsersDetailEntity

--- src/main/java/com/pavel/forumapplication/Entity/UsersDetailEntity.java
+++ src/main/java/com/pavel/forumapplication/Entity/UsersDetailEntity.java
@@ -3,6 +3,7 @@ package com.pavel.forumapplication.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pavel.forumapplication.Enum.StatusEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
@@ -25,12 +26,14 @@ public class UsersDetailEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Size(min = 3, max = 16, message = "name min 3, max 16")
     private String name;
+    @Size(min = 3, max = 18, message = "surname min 3, max 18")
     private String surname;
 
     private int age;
 
+    @Size(min = 3, max = 302, message = "description min 3, max 302")
     private String description;
 
     private LocalDateTime birthday;
@@ -41,6 +44,7 @@ public class UsersDetailEntity {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     @NotAudited
+    @JsonIgnore
     private UsersEntity usersEntity;
 
     @OneToOne
