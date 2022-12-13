dcda2418562f6c03af3f6b68d7be7e2fa40d3414
pavel <deychuk@inbox.ru>
2022-12-13 19:15:37 +0300

    Change TopicEntity

--- src/main/java/com/pavel/forumapplication/Entity/TopicEntity.java
+++ src/main/java/com/pavel/forumapplication/Entity/TopicEntity.java
@@ -2,9 +2,13 @@ package com.pavel.forumapplication.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
 
 import java.util.List;
 
@@ -17,19 +21,27 @@ import java.util.List;
 @Table(name = "topic")
 @DynamicUpdate
 @DynamicInsert
+@Audited
 public class TopicEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 8, max = 36, message = "name min 8 max 36")
+    @NotNull(message = "name is null!")
     private String name;
 
+    @Size(min = 12, max = 350, message = "description min 8 max 350")
+    @NotNull(message = "description is null!")
     private String description;
 
     @ManyToMany(mappedBy = "topicEntities")
     @JsonIgnore
+    @NotAudited
     private List<CategoryEntity> categoryEntities;
 
     @OneToOne(mappedBy = "topicEntity", cascade = CascadeType.ALL)
+    @JsonIgnore
+    @NotAudited
     private ImageEntity imageEntity;
 }
