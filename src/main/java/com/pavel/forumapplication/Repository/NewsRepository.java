4d4756ad4b4b12dcaa6ac026d48758b461a4025d
pavel <deychuk@inbox.ru>
2022-12-13 19:08:51 +0300

    Change NewsRepository

--- src/main/java/com/pavel/forumapplication/Repository/NewsRepository.java
+++ src/main/java/com/pavel/forumapplication/Repository/NewsRepository.java
@@ -3,6 +3,9 @@ package com.pavel.forumapplication.Repository;
 import com.pavel.forumapplication.Entity.NewsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NewsRepository extends JpaRepository<NewsEntity,Long> {
+    Optional<NewsEntity> findByName(String name);
 
 }
