8fce0628d852014cc97e9574af961a5efc4b32f7
pavel <deychuk@inbox.ru>
2022-12-13 19:10:00 +0300

    Change ImageEntity

--- src/main/java/com/pavel/forumapplication/Entity/ImageEntity.java
+++ src/main/java/com/pavel/forumapplication/Entity/ImageEntity.java
@@ -1,8 +1,12 @@
 package com.pavel.forumapplication.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
 
 import java.sql.Types;
 
@@ -13,6 +17,7 @@ import java.sql.Types;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "image")
+@Audited
 public class ImageEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,23 +27,32 @@ public class ImageEntity {
 
     @Lob
     @JdbcTypeCode(Types.BINARY)
+    @NotAudited
     private byte[] bytes;
 
     private Long size;
 
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_detail_id")
+    @JsonIgnore
+    @NotAudited
     private UsersDetailEntity usersDetailEntity;
 
     @OneToOne
     @JoinColumn(name = "topic_id")
+    @NotAudited
+    @JsonIgnore
     private TopicEntity topicEntity;
 
     @OneToOne
     @JoinColumn(name = "comment_id")
+    @JsonIgnore
+    @NotAudited
     private CommentEntity commentEntity;
 
     @OneToOne
     @JoinColumn(name = "news_id")
+    @JsonIgnore
+    @NotAudited
     private NewsEntity newsEntity;
 }
