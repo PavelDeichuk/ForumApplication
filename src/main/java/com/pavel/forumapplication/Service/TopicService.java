991a56dba8d8ad6fecf26a8025dbc9bca1376b3f
pavel <deychuk@inbox.ru>
2022-12-13 19:16:05 +0300

    Change TopicService

--- src/main/java/com/pavel/forumapplication/Service/TopicService.java
+++ src/main/java/com/pavel/forumapplication/Service/TopicService.java
@@ -2,6 +2,7 @@ package com.pavel.forumapplication.Service;
 
 import com.pavel.forumapplication.Dto.TopicDto;
 import com.pavel.forumapplication.Entity.TopicEntity;
+import org.springframework.validation.BindingResult;
 
 import java.io.IOException;
 import java.util.List;
@@ -13,9 +14,9 @@ public interface TopicService {
 
     TopicDto AddTopicToCategory(Long topic_id, Long category_id);
 
-    TopicDto CreateTopic(TopicEntity topicEntity) throws IOException;
+    TopicDto CreateTopic(TopicEntity topicEntity, BindingResult bindingResult) throws IOException;
 
-    TopicDto EditTopic(Long topic_id, TopicEntity topicEntity);
+    TopicDto EditTopic(Long topic_id, TopicEntity topicEntity, BindingResult bindingResult);
 
     TopicDto DeleteTopic(Long topic_id);
 }
