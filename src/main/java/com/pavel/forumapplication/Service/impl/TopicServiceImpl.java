9bccf87213ed64c8c041f26605ae8147d3b5c921
pavel <deychuk@inbox.ru>
2022-12-13 19:16:24 +0300

    Change TopicServiceImpl

--- src/main/java/com/pavel/forumapplication/Service/impl/TopicServiceImpl.java
+++ src/main/java/com/pavel/forumapplication/Service/impl/TopicServiceImpl.java
@@ -3,16 +3,18 @@ package com.pavel.forumapplication.Service.impl;
 import com.pavel.forumapplication.Dto.TopicDto;
 import com.pavel.forumapplication.Entity.CategoryEntity;
 import com.pavel.forumapplication.Entity.TopicEntity;
-import com.pavel.forumapplication.Mapper.CategoryMapper;
+import com.pavel.forumapplication.Exception.BadRequestException;
+import com.pavel.forumapplication.Exception.NotFoundException;
 import com.pavel.forumapplication.Mapper.TopicMapper;
 import com.pavel.forumapplication.Repository.CategoryRepository;
 import com.pavel.forumapplication.Repository.TopicRepository;
-import com.pavel.forumapplication.Service.CategoryService;
 import com.pavel.forumapplication.Service.TopicService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import java.io.*;
 import java.util.List;
@@ -33,6 +35,9 @@ public class TopicServiceImpl implements TopicService {
     public List<TopicDto> GetAllTopic(int page, int size) {
         Pageable pageable = PageRequest.of(page,size);
         Page<TopicEntity> topicEntities = topicRepository.findAll(pageable);
+        if(topicEntities.isEmpty()){
+            throw new NotFoundException("topic list is empty!");
+        }
         return topicEntities
                 .stream()
                 .map(TopicMapper.INSTANCE::TOPIC_DTO)
@@ -69,7 +74,13 @@ public class TopicServiceImpl implements TopicService {
     }
 
     @Override
-    public TopicDto CreateTopic(TopicEntity topicEntity) throws IOException {
+    public TopicDto CreateTopic(TopicEntity topicEntity, BindingResult bindingResult) throws IOException {
+        if(bindingResult.hasErrors()){
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError error :  fieldErrors){
+                throw new BadRequestException(error.getObjectName() + " " + error.getDefaultMessage());
+            }
+        }
         TopicEntity topic = topicRepository
                 .saveAndFlush(
                         TopicEntity
@@ -82,12 +93,18 @@ public class TopicServiceImpl implements TopicService {
     }
 
     @Override
-    public TopicDto EditTopic(Long topic_id, TopicEntity topicEntity) {
+    public TopicDto EditTopic(Long topic_id, TopicEntity topicEntity, BindingResult bindingResult) {
         TopicEntity topic = topicRepository
                 .findById(topic_id)
                 .orElseThrow(() -> {
                     throw new RuntimeException("Not found for topic id!");
                 });
+        if(bindingResult.hasErrors()){
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError error :  fieldErrors){
+                throw new BadRequestException(error.getObjectName() + " " + error.getDefaultMessage());
+            }
+        }
         topic.setName(topicEntity.getName());
         topic.setDescription(topicEntity.getDescription());
         topicRepository.save(topic);
